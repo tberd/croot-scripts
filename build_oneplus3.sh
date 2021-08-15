8babf130c5726691404476ba085b058dbd2db75b
MSe1969 <mse1969@posteo.de>
2021-08-15 14:39:03 +0200

    Build-script: oneplus3
    Change-Id: I1564035c7804959e785199f3c703a265c46dd5c1

---
+++ build_oneplus3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Own values
+export CCACHE_DIR=~/out-android/.ccache
+export OUT_DIR_COMMON_BASE=~/out-android
+
+# Use pre-defined build script
+source z_patches/build_device.sh oneplus3 $1
