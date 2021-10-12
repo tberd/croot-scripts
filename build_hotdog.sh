80c55be6d4923183d0d760d72c2902d10ce8590b
MSe1969 <mse1969@posteo.de>
2021-10-12 22:46:40 +0200

    Build-script: hotdog
    Change-Id: I04317ef286cac9b24dc1a685696581c04f46a6b1

---
+++ build_hotdog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Own values
+export CCACHE_DIR=~/out-android/.ccache
+export OUT_DIR_COMMON_BASE=~/out-android
+
+# Use pre-defined build script
+source z_patches/build_device.sh hotdog $1
+
+
+
