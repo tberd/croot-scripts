e2858b72cd32e22bc9294b3f942f3eaad1402ffa
MSe <mse1969@posteo.de>
2022-05-07 22:27:54 +0200

    Build-script: hotdog - skip ABI checks
    Change-Id: I17727f33503f3ed748da55cb0889f219af2bb50b

--- build_hotdog.sh
+++ build_hotdog.sh
@@ -3,6 +3,7 @@
 # Own values
 export CCACHE_DIR=~/out-android/.ccache
 export OUT_DIR_COMMON_BASE=~/out-android
+export SKIP_ABI_CHECKS=true
 
 # Use pre-defined build script
 source z_patches/build_device.sh hotdog $1
