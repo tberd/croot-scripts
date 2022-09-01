5594b5ae01571add1f320430233b1bc52f57525d
MSe <mse1969@posteo.de>
2022-09-01 22:19:31 +0200

    Switch-script: new Contacts fork
    Change-Id: Icdab568b55af7786fce839c8cc488ecc03dd528e

--- switch_microG.sh
+++ switch_microG.sh
@@ -128,6 +128,7 @@ switch_branches $BRANCH1 frameworks/opt/net/wifi
 switch_branches $BRANCH5 libcore
 switch_branches $BRANCH1 packages/apps/Bluetooth
 switch_branches $BRANCH1 packages/apps/Camera2
+switch_branches $BRANCH1 packages/apps/Contacts
 switch_branches $BRANCH1 packages/apps/LineageParts
 switch_branches $BRANCH1 packages/apps/Nfc
 switch_branches $BRANCH1 packages/apps/Settings
